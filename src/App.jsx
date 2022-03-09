52dd54bf318cf9b5acae8b2e08a324e72949c1d7
Lucas Fernando <73972922+DigitalHDR@users.noreply.github.com>
2022-03-09 00:17:42 -0300

    rota dinâmica id para item único da API

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Home from './components/Home';
 import Navbar from './components/Navbar';
 import { Routes, Route } from 'react-router-dom'
 import Products from './components/Products';
+import Product from './components/Product';
 
 function App() {
   return (
@@ -10,7 +11,8 @@ function App() {
       <Navbar />
       <Routes>
         <Route index element={<Home />} />
-        <Route path="products" element={<Products />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/products/:id" element={<Product />} />
       </Routes>
     </>
   );
--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import Skeleton from 'react-loading-skeleton'
+import { NavLink } from 'react-router-dom'
 
 export default function Products() {
   const [data, setData] = useState([])
@@ -85,7 +86,7 @@ export default function Products() {
                 <div className="card-body">
                   <h5 className="card-title mb-0">{product.title.substring(0, 12)}</h5>
                   <p className="card-text lead fw-bold">{product.price}</p>
-                  <div href="#" className="btn btn-outline-dark">Go somewhere</div>
+                  <NavLink to={`/products/${product.id}`} className="btn btn-outline-dark">Buy Now</NavLink>
                 </div>
               </div>
             </div>
